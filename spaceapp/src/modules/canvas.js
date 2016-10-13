055aacadb510aa502ebbc805b0e09c74967c40c3
Stuart Laverick <appsol@gmail.com>
2016-10-13 11:07:59 +0100

    Stubbed out Particle.View.render
    Particle.View.render iterates over particle collection and renders each to the canvas

--- spaceapp/src/modules/canvas.js
+++ spaceapp/src/modules/canvas.js
@@ -13,17 +13,12 @@
                 this.width = this.el.width = window.innerWidth;
                 this.height = this.el.height = window.innerHeight;
                 this.context = this.el.getContext('2d');
+                var Particle = app.getModule('particle');
+                new Particle.View({context: this.context});
             },
             render: function()
             {
                 this.context.clearRect(0, 0, this.width, this.height);
             }
         });
-
-        return {
-            createView: function()
-            {
-                return new Canvas.View();
-            }
-        };
     })(app);
--- spaceapp/src/modules/particle.js
+++ spaceapp/src/modules/particle.js
@@ -30,29 +30,82 @@
         });
 
         Particle.List = Backbone.Collection.extend({
-            model: Particle.Model
+            model: Particle.Model,
+            // url: 'assets/js/particles.json'
         });
 
         Particle.View = Backbone.View.extend({
             el: '#canvas',
             initialize: function(viewData)
             {
-
+                this.context = viewData.context;
+                this.particles = new Particle.List(particleJson);
+                // this.particles.fetch();
+                this.render();
+            },
+            render: function()
+            {
+                // this.context.beginPath();
+                this.context.strokeStyle = "white";
+                this.context.lineWidth = 1;
+                this.context.beginPath();
+                this.particles.each (_.bind(function(particle)
+                    {
+                        var sticks = particle.get('s'),
+                            points = particle.get('p');
+                        // this.context.beginPath();
+                        _.forEach (sticks, _.bind(function(stick)
+                        {
+                            // this.context.beginPath();
+                            this.context.moveTo(points[stick.p0].x2, points[stick.p0].y2);
+                            this.context.lineTo(points[stick.p1].x2, points[stick.p1].y2);
+                            // this.context.stroke();
+                        }, this));
+                        // this.context.stroke();
+                    }, this));
+                this.context.stroke();
             }
         });
-
-        return {
-            createModel: function(modelData)
-            {
-                return new Particle.Model(modelData);
-            },
-            createList: function(modelsData)
-            {
-                return new Particle.List(modelsData);
-            },
-            createView: function(viewData)
-            {
-                return new Particle.View(viewData);
-            }
-        };
     })(app);
+
+var particleJson = [
+    {
+        "p": {
+            "1": {
+                "x1": 100,
+                "y1": 100,
+                "x2": 105,
+                "y2": 105
+            },
+            "2": {
+                "x1": 120,
+                "y1": 100,
+                "x2": 125,
+                "y2": 105
+            },
+            "3": {
+                "x1": 100,
+                "y1": 120,
+                "x2": 105,
+                "y2": 125
+            }
+        },
+        "s": {
+            "s1": {
+                "p0": 1,
+                "p1": 2,
+                "v": 1
+            },
+            "s2": {
+                "p0": 2,
+                "p1": 3,
+                "v": 1
+            },
+            "s3": {
+                "p0": 3,
+                "p1": 1,
+                "v": 1
+            }
+        }
+    }
+];
