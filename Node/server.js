1040c2634661423877008460d95585ec1642cfe9
Stuart Laverick <appsol@gmail.com>
2017-07-07 18:17:46 +0100

    Starting to play with Node

---
+++ Node/server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end("Hello World");
+});
+
+server.listen(8000);
